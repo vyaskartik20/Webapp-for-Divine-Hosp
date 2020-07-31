2732d24cfb253a2016fb7c69817776adcdc84c6b
vyaskartik20 <Kartik Vyas>
2020-07-31 19:58:41 +0530

    Updates

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,36 @@
       $(".navbar-collapse").collapse('hide');
     });
 
+  //   $('.counter').counterUp({
+  //     delay: 10,
+  //     time: 1000
+  // });
+
+//   $('.counter').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         delay:10,
+//         duration: 4000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
+
+// $('.counter').each(function () {
+//   $(this).prop('Counter',0).animate({
+//           Counter: $(this).text()
+//   }, {
+//           duration: 4000,
+//           easing: 'swing',
+//           step: function (now) {
+//                   $(this).text(Math.ceil(now));
+//           }
+//   });
+// });
+
 
     // Owl Carousel
     $('.owl-carousel').owlCarousel({
@@ -43,3 +73,42 @@
     new WOW({ mobile: false }).init();
 
 })(jQuery);
+
+
+
+/** 
+  * Template Name: Varsity
+  * Version: 1.0  
+  * Template Scripts
+  * Author: MarkUps
+  * Author URI: http://www.markups.io/
+
+  Custom JS
+  
+
+  1. SEARCH FORM
+  2. ABOUT US VIDEO
+  2. TOP SLIDER
+  3. ABOUT US (SLICK SLIDER) 
+  4. LATEST COURSE SLIDER (SLICK SLIDER) 
+  5. TESTIMONIAL SLIDER (SLICK SLIDER)
+  6. COUNTER
+  7. RELATED ITEM SLIDER (SLICK SLIDER)
+  8. MIXIT FILTER (FOR GALLERY)
+  9. FANCYBOX (FOR PORTFOLIO POPUP VIEW)  
+  11. HOVER DROPDOWN MENU
+  12. SCROLL TOP BUTTON  
+
+  
+**/
+
+jQuery(function($){
+
+    jQuery('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+});
+
